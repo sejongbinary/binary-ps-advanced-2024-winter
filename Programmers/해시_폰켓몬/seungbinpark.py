96d2e0606061fbf34e34036325dbb205a1e814e9
seungbinpark <70265096+seungbinpark@users.noreply.github.com>
2025-01-13 02:49:35 +0900

    [seungbinpark] programmers_폰켓몬_Python

--- Programmers/해시_폰켓몬/seungbinpark.py
+++ Programmers/해시_폰켓몬/seungbinpark.py
@@ -1,14 +1,7 @@
-from collections import Counter
-p = ["mislav", "stanko", "mislav", "ana"]
-c = ["stanko", "ana", "mislav"]
-
-ans=[]
-    
-com = Counter(c)
-    
-for name in p:
-    if c[name]>0:
-        c[name] -= 1
-    else:
-        ans.append(name)
-print(ans)
+def solution(nums):
+    n = set(nums) # 중복 제외 종류 번호의 집합
+    N = len(nums)/2 # 이론상의 최댓값
+    # 종류 번호의 종류가 N/2개보다 적을 때
+    if N>=len(n): return len(n) 
+    # 종류 번호의 종류가 N/2개보다 많을 때
+    elif N<len(n): return N
